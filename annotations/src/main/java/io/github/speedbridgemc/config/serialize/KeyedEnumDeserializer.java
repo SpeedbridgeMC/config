b69e2914c4d50b6d1417c20e6689f292af81093b
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-03-28 16:50:08 +0300

    Runtime: Fix javadoc for KeyedEnumDeserializer

--- annotations/src/main/java/io/github/speedbridgemc/config/serialize/KeyedEnumDeserializer.java
+++ annotations/src/main/java/io/github/speedbridgemc/config/serialize/KeyedEnumDeserializer.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * Marks a method to be used to deserialize a {@code KeyedEnum} value.<p>
  * Methods marked with this annotation should be public and static, take the enum's key type as its single parameter,
  * and return an enum value.<p>
- * Ignored outside of enums that implement that interface.
+ * Ignored outside of enums that don't implement {@code KeyedEnum}.
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.SOURCE)
