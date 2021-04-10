1957378b7cc133b15d403b6ea68d76d0c7398ef1
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-04-10 12:56:56 +0300

    Processor: add docs for MethodSignature
    Removed some unnecessary methods

--- processor/src/main/java/io/github/speedbridgemc/config/processor/api/MethodSignature.java
+++ processor/src/main/java/io/github/speedbridgemc/config/processor/api/MethodSignature.java
@@ -7,9 +7,12 @@ import org.jetbrains.annotations.Nullable;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.VariableElement;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.Optional;
 
+/**
+ * Represents a method signature.<p>
+ * Used for checking what methods the handler interface defines/implements.
+ */
 public final class MethodSignature {
     private final @NotNull String name;
     private final @NotNull TypeName @NotNull [] parameters;
@@ -23,6 +26,11 @@ public final class MethodSignature {
         this.returnType = returnType;
     }
 
+    /**
+     * Creates a {@code MethodSignature} that represents a {@link ExecutableElement}.
+     * @param method element to represent
+     * @return a signature based on the specified element
+     */
     public static @NotNull MethodSignature fromElement(@NotNull ExecutableElement method) {
         ArrayList<TypeName> parameters = new ArrayList<>();
         for (VariableElement parameter : method.getParameters())
@@ -31,41 +39,89 @@ public final class MethodSignature {
                 method.isDefault(), TypeName.get(method.getReturnType()).withoutAnnotations());
     }
 
+    /**
+     * Creates a {@code MethodSignature}.
+     * @param returnType return type of method
+     * @param name name of method
+     * @param parameters parameters of method
+     * @return a signature
+     */
     public static @NotNull MethodSignature of(@NotNull TypeName returnType, @NotNull String name, @NotNull TypeName... parameters) {
         return new MethodSignature(name, parameters, false, returnType);
     }
 
+    /**
+     * Creates a {@code MethodSignature} which represents a {@code default} method.
+     * @param returnType return type of method
+     * @param name name of method
+     * @param parameters parameters of method
+     * @return a signature
+     */
     public static @NotNull MethodSignature ofDefault(@NotNull TypeName returnType, @NotNull String name, @NotNull TypeName... parameters) {
         return new MethodSignature(name, parameters, true, returnType);
     }
 
+    /**
+     * Creates a {@code MethodSignature} which represents a {@code default} method with an unspecified return type.
+     * @param name name of method
+     * @param parameters parameters of method
+     * @return a signature
+     */
     public static @NotNull MethodSignature ofDefault(@NotNull String name, @NotNull TypeName... parameters) {
         return new MethodSignature(name, parameters, true, null);
     }
 
+    /**
+     * Gets the name of the method.
+     * @return method name
+     */
     public @NotNull String getName() {
         return name;
     }
 
+    /**
+     * Gets the method's parameters.
+     * @return method parameters
+     */
     public @NotNull TypeName @NotNull [] getParameters() {
         return parameters.clone();
     }
 
+    /**
+     * Checks if the method is a {@code default} method.
+     * @return {@code true} if {@code default}, {@code false} otherwise
+     */
     public boolean isDefault() {
         return isDefault;
     }
 
-    public @Nullable TypeName getReturnType() {
-        return returnType;
+    /**
+     * Gets the method's return type.
+     * @return method return type, or empty if unspecified
+     */
+    public @NotNull Optional<TypeName> getReturnType() {
+        return Optional.ofNullable(returnType);
     }
 
+    /**
+     * Checks if this method signature matches another signature.<p>
+     * Note that this is <em>not</em> an equality check.
+     * @param other other signature
+     * @return {@code true} if this signature and the other signature match, {@code false} otherwise
+     */
     public boolean matches(@NotNull MethodSignature other) {
         if (returnType == null)
-            return toStringWithoutReturnType().equals(other.toStringWithoutReturnType());
+            return toStringWithoutReturnType(true).equals(other.toStringWithoutReturnType(true));
         else
             return toString().equals(other.toString());
     }
 
+    /**
+     * Checks if a method signature has a matching signature in an {@link Iterable}.
+     * @param methods method iterable
+     * @param signature signature to check
+     * @return {@code true} if a match was found, {@code false} otherwise
+     */
     public static boolean contains(@NotNull Iterable<@NotNull ? extends MethodSignature> methods,
                                    @NotNull MethodSignature signature) {
         for (MethodSignature method : methods) {
@@ -75,28 +131,9 @@ public final class MethodSignature {
         return false;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        MethodSignature that = (MethodSignature) o;
-        return isDefault == that.isDefault && name.equals(that.name)
-                && Arrays.equals(parameters, that.parameters)
-                && Objects.equals(returnType, that.returnType);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(name, isDefault, returnType);
-        result = 31 * result + Arrays.hashCode(parameters);
-        return result;
-    }
-
     private @Nullable String cachedNRTString, cachedFullString;
 
-    public @NotNull String toStringWithoutReturnType(boolean addDefault) {
+    private @NotNull String toStringWithoutReturnType(boolean addDefault) {
         if (cachedNRTString == null) {
             StringBuilder sb = new StringBuilder(name).append('(');
             for (TypeName param : parameters)
@@ -111,10 +148,10 @@ public final class MethodSignature {
             return cachedNRTString;
     }
 
-    public @NotNull String toStringWithoutReturnType() {
-        return toStringWithoutReturnType(true);
-    }
-
+    /**
+     * Returns this signature as a {@link String}.
+     * @return {@code String} representation
+     */
     @Override
     public @NotNull String toString() {
         if (cachedFullString == null) {
