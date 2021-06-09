b24c4b3dfad7be3de38ed18b0d239a90bf55f0fb
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-06-09 21:09:52 +0300

    stupid generated annotation

--- processor/src/main/java/io/github/speedbridgemc/config/processor/api/generation/CodeGenerator.java
+++ processor/src/main/java/io/github/speedbridgemc/config/processor/api/generation/CodeGenerator.java
@@ -1,5 +1,6 @@
 package io.github.speedbridgemc.config.processor.api.generation;
 
+import com.squareup.javapoet.ClassName;
 import io.github.speedbridgemc.config.processor.api.ProcessingWorker;
 import io.github.speedbridgemc.config.processor.api.type.ConfigType;
 import org.jetbrains.annotations.NotNull;
@@ -7,5 +8,11 @@ import org.jetbrains.annotations.NotNull;
 import javax.lang.model.element.TypeElement;
 
 public interface CodeGenerator extends ProcessingWorker {
-    void generateFor(@NotNull ConfigType rootType, @NotNull TypeElement rootElement);
+    interface Context {
+        @NotNull ClassName generatedAnnotation();
+        @NotNull String date();
+        @NotNull String processorVersion();
+    }
+
+    void generateFor(@NotNull Context ctx, @NotNull ConfigType rootType, @NotNull TypeElement rootElement);
 }
