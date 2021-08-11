cc6e690637adf722dbfb9757fb22ca99a74415ab
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-08-11 12:44:06 +0300

    that isn't how code generators work

--- handler-itf/src/main/java/io/github/speedbridgemc/config/handleritf/impl/HandlerItfImplGenerator.java
+++ handler-itf/src/main/java/io/github/speedbridgemc/config/handleritf/impl/HandlerItfImplGenerator.java
@@ -1,13 +1,10 @@
 package io.github.speedbridgemc.config.handleritf.impl;
 
-import com.google.auto.service.AutoService;
 import io.github.speedbridgemc.config.processor.api.generation.BaseCodeGenerator;
-import io.github.speedbridgemc.config.processor.api.generation.CodeGenerator;
 import io.github.speedbridgemc.config.processor.api.type.ConfigType;
 
 import javax.lang.model.element.TypeElement;
 
-@AutoService(CodeGenerator.class)
 public final class HandlerItfImplGenerator extends BaseCodeGenerator {
     @Override
     public void generateFor(Context ctx, ConfigType rootType, TypeElement rootElement) {
